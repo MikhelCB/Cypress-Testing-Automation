e88b458c4a19c52a8497b9459045bc2df49156b9
Mikhel Cabral <srrynow@hotmail.com>
2025-01-30 15:11:58 -0300

    feat: include intercepts in API and testing requests of APIs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,38 @@
 describe('Aprendendo conceitos Cypress', () => {
   it('1 - Usuário faz login com username e senha inválida', () => {
     cy.visit('/')
-    cy.get('div.shop-menu').contains('Login').click()
-    cy.get('[data-qa="login-email"]').type('teste@gmail.com')
-    // também funciona o cy.get(.login-email(nome da div)).find('input[name="email"]') pega a div, e dentro da div encontra o input que contém o name=email
-    cy.get('[data-qa="login-password"]').type('12345')
-    cy.get('[data-qa="login-button"]').contains("Login").click()
+    cy.get('div.shop-menu').contains('Login').should('have.attr', 'href', '/login').click()
+    cy.get('[data-qa="login-email"]')
+    .type('teste@gmail.com')
+    .should('be.visible')
+    .and('have.attr', 'placeholder', 'Email Address')
+    .and('have.prop','required')
 
-    cy.contains('Your email or password is incorrect!')
+
+    // também funciona o cy.get(.login-email(nome da div)).find('input[name="email"]') pega a div, e dentro da div encontra o input que contém o name=email
+    cy.get('[data-qa="login-password"]').type('12345').should('have.value', '12345')
+
+    //     utilizando o .then
+    // cy.get('[data-qa="login-button"]').as('btnLogin').then(($button) =>{
+    //   expect($button).to.have.text('Login')
+    //   expect($button).to.contain('Login')
+    //   expect($button).to.be.visible
+    //   expect($button).to.have.attr('type', 'submit')
+    //   expect($button).to.have.class('btn')
+
+    //   cy.wrap($button).click()
+    // })
+
+
+    cy.get('[data-qa="login-button"]').should(($button) =>{
+      expect($button).to.have.text('Login')
+      expect($button).to.contain('Login')
+      expect($button).to.be.visible
+      expect($button).to.have.attr('type', 'submit')
+      expect($button).to.have.class('btn')
+    }).click()
+
+    cy.contains('Your email or password is incorrect!').should('be.visible')
   })
   
   it('2- Acessando home da página automation Exercise', () => {
@@ -34,14 +59,25 @@ describe('Aprendendo conceitos Cypress', () => {
     cy.get('button.close-modal', {timeout: 5000}).click()
   })
 
-  it.only('5- Acessando a página de produtos usando intercept', () => {
+  it('5- Acessando a página de produtos usando intercept', () => {
     cy.visit('/')
-    cy.intercept('GET', 'products')
-    cy.get('.navbar-nav').contains('Products').click()
+    cy.intercept('GET', '/products').as('getProdutos')
+
+    cy.get('.navbar-nav').contains('Products').then(($btn) =>{
+      cy.wrap($btn).click()
+    })
+
+    cy.wait('@getProdutos').its('response.statusCode').should('eq', 200)
   })
 
-  it('6 - GET Produtos - usando resquest', () => {
-    cy.request('GET', 'api/productsList')
+  it.only('6 - GET produtos retorna 200 - usando request', () => {
+    cy.request('GET', 'api/productsList').should((response)=>{
+      expect(response.status).to.be.eq(200)
+      expect(response.body).not.to.be.empty
+      let body = JSON.parse(response.body)
+      expect(body.products).to.be.an('array')
+      expect(body.products).to.have.length.above(1)
+    })
     //comment for test
   })
 })
